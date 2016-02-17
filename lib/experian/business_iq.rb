7762067dc14b000b8b3bd09eac3c67730c992950
bjacobso <ben.jacobson@confidentfs.com>
2016-02-17 16:26:45 -0700

    requiring the correct request files

--- lib/experian/business_iq.rb
+++ lib/experian/business_iq.rb
@@ -1,5 +1,6 @@
 require 'experian/business_iq/client'
 require 'experian/business_iq/request'
+require 'experian/business_iq/business_request'
 require 'experian/business_iq/premier_request'
 require 'experian/business_iq/similars_request'
 require 'experian/business_iq/response'
@@ -24,19 +25,6 @@ module Experian
     def self.get_list_of_similars(options = {})
       Client.new.get_list_of_similars(options)
     end
-
-    # # convenience method
-    # def self.check_id(options = {})
-    #   Client.new.check_id(options)
-    # end
-    #
-    # def self.request_questions(options = {})
-    #   Client.new.request_questions(options)
-    # end
-    #
-    # def self.send_answers(options = {})
-    #   Client.new.send_answers(options)
-    # end
 
   end
 end
