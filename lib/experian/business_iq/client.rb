77e68daa82665f40f96106dd951185c60025e178
bjacobso <ben.jacobson@confidentfs.com>
2016-03-24 12:01:45 -0600

    fixing response request returns

--- lib/experian/business_iq/client.rb
+++ lib/experian/business_iq/client.rb
@@ -4,30 +4,43 @@ module Experian
 
       def get_profile(options = {})
         assert_check_options(options)
-        Response.new(submit_request(ProfileRequest.new(options)).body)
+        submit_request(ProfileRequest.new(options))
       end
 
       def get_business_profile(options = {})
-        options[:request_type] = :premier
+        options[:request_type] = :business
         assert_check_options(options)
-        Response.new(submit_request(ProfileRequest.new(options)).body)
+        submit_request(ProfileRequest.new(options))
       end
 
       def get_premier_profile(options = {})
-        options[:request_type] = :business
+        options[:request_type] = :premier
         assert_check_options(options)
-        Response.new(submit_request(ProfileRequest.new(options)).body)
+        submit_request(ProfileRequest.new(options))
       end
 
       def get_list_of_similars(options = {})
         assert_check_options(options)
-        Response.new(submit_request(SimilarsRequest.new(options)).body)
+        submit_request(SimilarsRequest.new(options))
+      end
+
+      private
+
+      def submit_request(request)
+        raw_response = super
+        response = Response.new(raw_response.body)
+        check_response(response, raw_response)
+        [request, response]
+      end
+
+      def check_response(response, raw_response)
+        if Experian.logger && response.error? && (response.error_code.nil? && response.error_message.nil?)
+          Experian.logger.debug "Unknown Experian Error Detected, Raw response: #{raw_response.inspect}"
+        end
       end
 
       def assert_check_options(options)
-        # return if options[:first_name] && options[:last_name] && options[:ssn]
-        # return if options[:first_name] && options[:last_name] && options[:street] && options[:zip]
-        # raise Experian::ArgumentError, "Required options missing: first_name, last_name, ssn OR first_name, last_name, street, zip"
+        # TODO
       end
 
     end
