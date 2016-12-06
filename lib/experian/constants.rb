8044464fe7a27c5584b611a63e93bc147b3fdd28
Fernando Juarez <fjuarez@applaudostudios.com>
2016-12-06 14:04:23 -0600

    change precise id url test

--- lib/experian/constants.rb
+++ lib/experian/constants.rb
@@ -7,7 +7,8 @@ module Experian
     PASSWORD_RESET_URL = "https://ss3.experian.com/securecontrol/reset/passwordreset"
     
     PRECISE_ID_URL = "https://ss3.experian.com/fraudsolutions/xmlgateway/preciseid"
-    PRECISE_ID_TEST_URL = "https://dm2.experian.com/fraudsolutions/xmlgateway/preciseid"
+    # PRECISE_ID_TEST_URL = "https://dm2.experian.com/fraudsolutions/xmlgateway/preciseid"
+    PRECISE_ID_TEST_URL = "https://dm-sgw1.experian.com/fraudsolutions/xmlgateway/preciseid"
 
     LOOKUP_SERVICE_NAME = "AccessPoint"
     LOOKUP_SERVICE_VERSION = 1.0
